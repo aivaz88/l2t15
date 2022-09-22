a12967003c91eeca2748c7e3f7fc7ea8068eb070
aivaz88 <aivaz1988@mail.ru>
2022-09-23 01:01:00 +0500

    comment

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.Write("Введите положительное число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num > 0 && num < 6)
+{
+    Console.WriteLine("Нет");
+}
+else if (num > 5 && num < 8)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет такого дня недели");
+}
